ea5db956b5b3a5da65a06d24adf5f1514bc9f0a5
Dan Victor DEPED <danvictorprogramming1@gmail.com>
2022-04-28 17:47:31 +0800

    Added BoilerPlate of the Game

---
+++ script.js
@@ -0,0 +1,10 @@
+function botGetRandomInt() {
+    return Math.floor(Math.random() * 100);
+}
+console.log(botGetRandomInt());
+let game_active = true;
+// Initializing DOM
+let guessInput = document.querySelector('.guess');
+while(game_active) {
+    
+}
